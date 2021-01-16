01b1a585da764afb08d6e3a8a390799f49bf03a8
Jelena Feliciano <jfel217@gmail.com>
2021-01-16 16:26:02 -0500

    Update script.js
    Validation method is working for each field except month and year because I didn't attempt that the last project and it's a little different.

--- js/script.js
+++ js/script.js
@@ -87,16 +87,14 @@ activities.addEventListener("change", (e) => {
 /********* payment section ********/
 //variables for separating the payment methods
 const payments = document.querySelector("#payment");
-const paymentOption = document.querySelectorAll("#payment option");
 const creditCard = document.querySelector("#credit-card");
 const payPal = document.querySelector("#paypal");
 const bitcoin = document.querySelector("#bitcoin");
-paymentOption[0].hidden = true;
-payments.value = "credit card";
+payments.value = "credit-card";
 payPal.style.display = "none";
 bitcoin.style.display = "none";
 //show/hide pym options depending on the users selection
-payment.addEventListener("change", (e) => {
+payments.addEventListener("change", (e) => {
   if (e.target.value === "credit-card") {
     creditCard.style.display = "block";
     payPal.style.display = "none";
@@ -116,37 +114,23 @@ payment.addEventListener("change", (e) => {
 });
 
 /** form Validation section */
-function validationPass(el) {
-  el.parentElement.className = 'error-border';
-}
-// function createError(formField) {
-//   if(!formField.previousElementSibling.classList.contains('error-border')) {
-//     const errorMessage = documents.getElementByClass('hint');
 
-//   }
-  
-// }
-// function removeError(error) {
-//   if(formField.previousElementSibling.classList.contains('error-border')) {
-//     formField.previousElementSibling.remove('');
-//     formField.setAttribute('required', false);
-
-//   }
-//}
 function isNameValid() {
   const nameInput = document.getElementById("name");
-
+  const nameError = document.getElementById('name-hint');
   if(nameInput.value === '') {
-    document.getElementById('name-hint');
-    document.querySelector('.error-border').style.display.color = 'red';
+    nameError.style.display = 'block';
+    nameInput.parentElement.classList.add('not-valid');
     return false;
   } 
   if(nameInput.value.search(/\d/) != -1) {
-    document.querySelector('.error-border');
-    document.getElementById('name-hint').textContent = 'Names should be letters only';
+    nameError.textContent = 'Names should be letters only';
     return false;
   } else {
-    
+    nameInput.parentElement.classList.remove('not-valid');
+    nameInput.parentElement.classList.add('valid');
+    nameError.style.display = 'none';
+
     return true;  
   }
 }
@@ -154,90 +138,120 @@ function isNameValid() {
 function isEmailValid() {
      const regEmail = /^[^@]+@[^@.]+\.[\w+]+$/i;
      const emailInput = document.getElementById("email");
+     const emailError = document.getElementById('email-hint');
     if(emailInput.value === '') {
-      removeError(emailInput);
-      createError(emailInput, '* Email is Empty');
+      emailError.style.display = 'block';
+      emailInput.parentElement.classList.add('not-valid');
       return false;
     } 
     if (!regEmail.test(emailInput.value)) {
-      removeError(emailInput);
-      createError(emailInput, '* Please enter a valid Email Address');
+    emailInput.parentElement.classList.add('not-valid');
+    emailError.style.display = 'block';      
       return false;
     } else {
-      removeError(emailInput);
+      emailError.style.display = 'none';
+      emailInput.parentElement.classList.remove('not-valid');
+      emailInput.parentElement.classList.add('valid');
       return true;
     }
    }
 
-const checkbox1 = document.querySelectorAll(".activities label")[0];
 function checkActivities() {
+  const activityError = document.getElementById('activities-hint');
   for (let i = 0; i < checkboxes.length; i++) {
-    if (!checkboxes[i].checked) {
-      checkboxes[i].setAttribute("required", true);
-      createError(activities, '*Please select');
+    if (!totalCost) {
+      activities.parentElement.classList.add('not-valid');
+      activityError.style.display = 'block';
       return false;
     } else {
+      activities.parentElement.classList.remove('not-valid');
+      activities.parentElement.classList.add('valid');
+      activityError.style.display = 'none';
       return true;
     }
   }
 }
 
-// function isCcValid() {
-//   const ccNum = document.getElementById("cc-num");
-//   const regexNums = /^(\d{13,16})$/;
-//   const zip = document.getElementById("zip");
-//   const regexZip = /\b\d{5}\b/;
-//   const cvvInput = document.getElementById("cvv");
-//   const regexCvv = /^\d{3}$/;
-//   if(payments.value === 'credit card') {
-//     let validate = true;
-//     if(!regexNums.test(ccNum.value)) {
-//       createError(ccNum, '*Please Enter a Valid Credit Card');
-//       validate = false;
-//     } else {
-//       removeError(ccNum);
-//     }
-//     if(!regexZip.test(zip.value)){
-//       createError(zip, '*Invalid zip code');
-//       validate = false;
-//     } else {
-//       removeError(zip);
-//     }
-//     if(!regexCvv.test(cvvInput.value)) {
-//       createError(cvvInput, '*3 digits on back')
-//       validate = false;
-//     } else {
-//       removeError(cvvInput);
-//     }
-//     if(validate === true) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-// //an eventlistener that calls tha validate funtions and verifies fieldsets
-// document.getElementById("name").addEventListener("keyup", isNameValid);
-// document.getElementById("email").addEventListener("keyup", isEmailValid);
-// document.getElementById("cc-num").addEventListener("keyup", isCcValid);
-// document.getElementById('zip').addEventListener('keyup', isCcValid);
-// document.getElementById('cvv').addEventListener('keyup', isCcValid)
+function isCcValid() {
+  const ccNumInput = document.getElementById("cc-num");
+  const regexNums = /^(\d{13,16})$/;
+  const zip = document.getElementById("zip");
+  const regexZip = /\b\d{5}\b/;
+  const cvvInput = document.getElementById("cvv");
+  const regexCvv = /^\d{3}$/;
+  const ccError = document.getElementById('cc-hint');
+  const zipError = document.getElementById('zip-hint');
+  const cvvError = document.getElementById('cvv-hint');
+  const expMon = document.querySelectorAll('#exp-month');
+  const expYear = document.querySelectorAll('#exp-year');
+  console.log(expMon);
+  console.log(expYear);
+   for(const month of expMon) {
+     console.log(month);
+   }
+  if(payments.value === 'credit-card') {
+    let validate = true;
+    if(!regexNums.test(ccNumInput.value)) {
+      ccError.style.display = 'block';
+      ccNumInput.parentElement.classList.add('not-valid');
+      validate = false;
+    } else {
+      ccNumInput.parentElement.classList.remove('not-valid');
+      ccNumInput.parentElement.classList.add('valid');
+      ccNumInput.classList.remove('error-border');
+      ccError.style.display = 'none'; 
+      validate = true;
+    }
+    if(!regexZip.test(zip.value)){
+      zipError.style.display = 'block';
+      zip.parentElement.classList.add('not-valid');
+
+      validate = false;
+    } else {
+      zipError.style.display = 'none';
+      zip.classList.remove('error-border');
+      zip.parentElement.classList.remove('not-valid');
+      zip.parentElement.classList.add('valid');
+
+      validate = true;
+
+    }
+    if(!regexCvv.test(cvvInput.value)) {
+      cvvInput.parentElement.classList.add('not-valid');
+      cvvError.style.display = 'block';
+      validate = false;
+    } else {
+      cvvInput.parentElement.classList.remove('not-valid');
+      cvvInput.style.display = 'none';
+      cvvInput.parentElement.classList.add('valid');
+
+    }
+    if(validate === true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return true;
+ }
+//an eventlistener that calls tha validate funtions and verifies fieldsets
+document.getElementById("name").addEventListener("keyup", isNameValid);
+document.getElementById("email").addEventListener("keyup", isEmailValid);
+document.getElementById("cc-num").addEventListener("keyup", isCcValid);
+document.getElementById('zip').addEventListener('keyup', isCcValid);
+document.getElementById('cvv').addEventListener('keyup', isCcValid)
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const validate = form.setAttribute('validate', true);
-  isNameValid() 
- 
-  // if (isNameValid() && isEmailValid() && isCcValid() && checkActivities()) {
-  //   alert('registration is Complete');
-  // } else {
-  //   e.preventDefault();
+  if (isNameValid() && isEmailValid() && isCcValid() && checkActivities()) {
+    alert('registration is Complete');
+  } else {
+    e.preventDefault();
 
-  //   isNameValid();
-  //   isEmailValid();
-  //   isCcValid();
-  //   checkActivities();
+    isNameValid();
+    isEmailValid();
+    isCcValid();
+    checkActivities();
 
-  // }
-  alert('submit button works')
+  }
+  //alert('submit button works')
 });
